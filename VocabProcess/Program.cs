cb3f12c963d6f570b170b8c862ebe84272a8fc81
MA86 <22569153+MA86@users.noreply.github.com>
2019-08-20 15:38:22 -0400

    Initial commit

---
+++ VocabProcess/Program.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VocabProcess
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// This console application processes a file of strings in the following format: 'WORD\n\nTRANSLATION\n\n' 
+        /// and outputs another file in the following format: 'WORD\tTRANSLATION\n'. Use CLI to run this application.
+        /// 
+        /// EXAMPLE:
+        /// 
+        /// Input File: 
+        /// communicable
+        /// 
+        /// ساري
+        /// 
+        /// Output File:
+        /// communicable	ساري
+        /// 
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            string vocabList = "";
+            bool englishWordCheck = false;
+            string word;
+
+            StreamReader sr = File.OpenText(args[0]);
+
+            // Get a list of vocab in this form: 'enlish\tforeign\n'
+            while (!sr.EndOfStream)
+            {
+                word = sr.ReadLine();
+
+                // Process english word if foreign word has already been processed
+                if (word != "" && englishWordCheck == false)
+                {
+                    vocabList += word + "\\t";
+
+                    // English word processed
+                    englishWordCheck = true;
+
+                    continue;
+                }
+
+                // Process foreign word if English word has already been processed
+                if (word != "" && englishWordCheck == true)
+                {
+                    vocabList += word + "\\n";
+
+                    // Foreign word processed
+                    englishWordCheck = false;
+
+                    continue;
+                }
+            }
+
+            sr.Close();
+
+            // Write the vocabList to a file on desktop
+            File.WriteAllText("vocabProcessed.txt", vocabList);
+        }
+    }
+}
